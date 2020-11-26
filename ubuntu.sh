6df0f3957c766348f1971050ce26dfb97afab2f0
samuelr357 <Samuel.oficial7@gmail.com>
2020-11-25 23:53:22 -0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -117,7 +117,9 @@ chmod +x *.AppImage
 
 ##CONFIGURAR VPN PARA INICIAR COM SISTEMA
 echo -e "\n$echo1\nMOSTRAR "Advanced Network Configuration" NO GNOME.\n$echo2\n"
-sudo sed '13d' /usr/share/applications/nm-connection-editor.desktop
+sudo sed '13d' /usr/share/applications/nm-connection-editor.desktop > /usr/share/applications/nm-connection-editor1.desktop
+sudo rm /usr/share/applications/nm-connection-editor.desktop
+vm /usr/share/applications/nm-connection-editor1.desktop /usr/share/applications/nm-connection-editor.desktop
 
 apt dist-upgrade -y
 apt autoclean 
