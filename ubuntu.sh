c267c7ead59e22b7b0847aa74c5d727868a155f9
samuelr357 <Samuel.oficial7@gmail.com>
2021-01-24 17:54:04 -0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -16,7 +16,7 @@ sudo apt full-upgrade -y  ;
 
 echo -e "\n$echo1\nDefinindo dependencias.\n$echo2\n"
 pacotes_dep=(curl wget  sed jq unzip chrome-gnome-shell net-tools gnome-tweak-tool git snapd build-essential default-jdk libssl-dev exuberant-ctags ncurses-term  silversearcher-ag fontconfig imagemagick libmagickwand-dev software-properties-common )
-pacotes_apt=(gimp vlc libreoffice netbeans qbittorrent enpass stacer steam insync gnome-boxes timeshift openjdk-8-jdk-headless gparted)
+pacotes_apt=(gimp vlc libreoffice grub-customizer netbeans qbittorrent enpass stacer steam insync gnome-boxes timeshift openjdk-8-jdk-headless gparted)
 pacotes_apt_recomendados=( wine-stable wine32 winetricks)
 
 ppas=()
